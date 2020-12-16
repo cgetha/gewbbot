e31b6c10c1240587f9bc7d8212e2c31dc1cbb4a4
cgetha <caseygetha@gmail.com>
2020-12-16 14:16:28 -0500

    up the creek

--- src/bot.js
+++ src/bot.js
@@ -9,38 +9,71 @@ var list = [
     "you fuckin wanka", 
     "ur not my dad", 
     "come at me bro", 
-    "fucking barry's hand dude"
+    "barry's hand dude"
 ]
-client.on('ready', () => {
-    console.log(`${client.user.tag} has logged in`)
+
+
+/* var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "IanG!060701!"
 });
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query("CREATE DATABASE mydb", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
+}); */
+
+client.on('ready', function () {
+        console.log(`${client.user.tag} has logged in`);
+    });
+
+
 client.on('message', (message) =>{
     if (message.author.bot) return;
-    fs.appendFile('output.txt',`/n ${message.content} : ${message.author.tag} : ${message.createdAt} \n`, error)
+    fs.appendFile('output.txt',`\n ${message.content} \n`, error)
 });
+
+
 client.on('message', (message) =>{
     if (message.author.bot) return;
     if (message.content.includes('fuck'))
     message.reply('watch your fucking mouth asshole.')
 });
+
+
 client.on('message', (message) =>{ /* weed is tight */
     if (message.author.bot) return;
     if (message.content.includes('weed'))
     message.reply('weed is tight bro, weed is tight.')
 });
+
+
 client.on('message', (message) =>{
     if (message.author.bot) return;
     if (message.content.includes('steve'))
         message.channel.send('<@142454868806729728> THE Professional Jon Simp');
 });
+
+
 client.on('message', (message) =>{
-    console.log(`${message.author.tag} sent a dumbass message`)
     if (message.content === 'hello')
     message.reply('fuck you asshole');
 });
+
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
+
+
 var selection = getRandomInt(list.length);
 client.on('message', (message) =>{
     if (message.author.bot) return;
@@ -48,25 +81,35 @@ client.on('message', (message) =>{
         message.reply(list[selection])
     }
 });
+
+
 /* 
 dont forget to comment ur worthless stuff
 */
+
+
 client.on('message', (message) =>{ /* bye response */
     console.log(`${message.author.tag} sent a dumbass message`)
     if (message.content === 'bye')
     message.reply('get fucked, loser.')
 });
+
+
 client.on('message', (message) =>{
     console.log(`${message.author.tag} sent a dumbass message`)
     if (message.content.includes('matt'))
     message.reply('fuckin a right bud')
 });
+
+
 client.on('message', (message) =>{ /* Chad response */
     if (message.author.bot) return;
     console.log(`${message.author.tag} sent a dumbass message`)
     if (message.content.includes('Tyler'))
     message.reply('Tyler? Did you mean Chad?')
 });
+
+
 client.on('message', (message) =>{ /* Array Destructure & PREFIX implementation */
     if (message.author.bot) return;
     if (message.content.startsWith(PREFIX)) {
@@ -80,6 +123,8 @@ client.on('message', (message) =>{ /* Array Destructure & PREFIX implementation 
         }
     }
 });
+
+
 client.on('message', (message) =>{ /* Array Destructure & PREFIX implementation */
     if (message.author.bot) return;
     if (message.content.startsWith(PREFIX)) {
@@ -93,6 +138,8 @@ client.on('message', (message) =>{ /* Array Destructure & PREFIX implementation 
         }
     }
 });
+
+
 client.on('message', (message) =>{ /* Array Destructure & PREFIX implementation */
     if (message.author.bot) return;
     if (message.content.startsWith(PREFIX)) {
@@ -102,7 +149,7 @@ client.on('message', (message) =>{ /* Array Destructure & PREFIX implementation 
         .split(/\s+/);
 
         if (CMD_NAME === 'love'){
-        message.channel.send("https://tenor.com/view/ha-gay-ken-jeong-youre-gay-thats-gay-gif-16371455");
+        message.channel.send("https://tenor.com/view/ha-gay-yeah-right-ha-yeah-ha-hayyyy-gif-11812798");
         }
     }
 });
