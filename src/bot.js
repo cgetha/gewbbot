7a0b3efc59b1cd6b92bc045c12f2a7afd3a96f76
Casey Getha <69476885+cgetha@users.noreply.github.com>
2020-12-16 14:29:50 -0500

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -13,24 +13,6 @@ var list = [
 ]
 
 
-/* var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "IanG!060701!"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  con.query("CREATE DATABASE mydb", function (err, result) {
-    if (err) throw err;
-    console.log("Database created");
-  });
-}); */
-
 client.on('ready', function () {
         console.log(`${client.user.tag} has logged in`);
     });
